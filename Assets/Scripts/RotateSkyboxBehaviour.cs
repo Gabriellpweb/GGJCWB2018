4194a0d4e35ca1b61cd78d100347ed1ffc5d0d27
gabriel luiz pereira <gabrielluizpereira@MacBook-Pro-de-gabriel.local>
2018-01-27 02:52:13 -0200

    Adicionado skybox rotativo

---
+++ Assets/Scripts/RotateSkyboxBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSkyboxBehaviour : MonoBehaviour {
+
+	[SerializeField]
+	private Material skyBoxMaterial;
+
+	[SerializeField]
+	private int rotateInterval;
+
+	private float rotateTimer;
+
+	private float rotation = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		RenderSettings.skybox = skyBoxMaterial;
+		rotateTimer = rotateInterval;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		skyBoxMaterial.SetFloat ("_Rotation", rotation);
+		rotateTimer--;
+
+		if (rotateTimer <= 0) {
+			rotation += Time.deltaTime;
+			rotateTimer = rotateInterval;
+		}
+	}
+}
