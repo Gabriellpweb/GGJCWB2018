345a3d39794c1ce5417f5c9a6cc9aa5cb2621630
gabriel luiz pereira <gabrielluizpereira@MacBook-Pro-de-gabriel.local>
2018-01-27 19:57:04 -0200

    Mecanica de tiro

--- Assets/Scripts/SatelliteBehaviour.cs
+++ Assets/Scripts/SatelliteBehaviour.cs
@@ -7,6 +7,14 @@ public class SatelliteBehaviour : MonoBehaviour {
 	[SerializeField]
 	private Transform referenceObjectToAngle;
 
+	[SerializeField]
+	private Transform shotOriginPoint;
+
+	[SerializeField]
+	private GameObject basicMissile;
+
+	private bool canFire = true;
+
 	private float angle = 0;
 
 	// Use this for initialization
@@ -18,10 +26,27 @@ public class SatelliteBehaviour : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		//angle = Vector3.Angle (
-		//	              referenceObjectToAngle.position, 
-		//	              transform.position);
+		if (Input.GetButtonDown("Fire1") && canFire) {
+			Fire ();
+			//canFire = false;
+		}
+
 		angle = Mathf.Atan2(transform.position.y, transform.position.x) * Mathf.Rad2Deg;
 		transform.rotation = Quaternion.Euler (new Vector3 (0, 0, angle));
 	}
+
+	void Fire()
+	{
+		Vector3 direction = new Vector3(
+			5f * Mathf.Cos((angle * Mathf.PI) / 180f) + transform.position.x,
+			5f * Mathf.Sin((angle * Mathf.PI) / 180f) + transform.position.y,
+			0
+		);
+
+		GameObject missile = GameObject.Instantiate (basicMissile, shotOriginPoint.transform.position, Quaternion.Euler(direction));
+		MissileBase missileBase = missile.GetComponent<MissileBase> ();
+		missileBase.WithForce (direction);
+		missileBase.Fire ();
+	}
+
 }
